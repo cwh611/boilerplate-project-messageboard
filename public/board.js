405c6198473b969f4f1600a202ead3ecf87aa7d4
cwh611 <chuck@MacBookPro.lan>
2025-02-06 18:57:49 -0800

    updates

--- public/board.js
+++ public/board.js
@@ -1,7 +1,9 @@
+const path = window.location.pathname;
+const match = path.match(/\/b\/([^\/]+)/);
+const board = match[1];
+
 document.addEventListener("DOMContentLoaded", async () => {
-    const path = window.location.pathname;
-    const match = path.match(/\/b\/([^\/]+)/);
-    const board = match[1];
+    document.getElementById("h1-dynamic-child").innerText = board;
     try {
         const response = await fetch(`https://chunk-messageboard-09594f5bef7e.herokuapp.com/api/threads/${board}`);
         const data = await response.json();
@@ -10,7 +12,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 `<div class="thread-container">
                     <span class="thread-title">
                         <strong>
-                            <a href="/b/${board}/${thread._id}">
+                            <a href="https://chunk-messageboard-09594f5bef7e.herokuapp.com/b/${board}/${thread._id}">
                                 ${thread.text}
                             </a>
                         </strong>
@@ -38,4 +40,52 @@ document.addEventListener("DOMContentLoaded", async () => {
     } catch (err) {
         alert("Server error loading threads:", err)
     }
+});
+
+document.getElementById("create-thread-btn").addEventListener("click", async () => {
+    let thread_title = document.getElementById("thread-title-input").value;
+    let thread_password = document.getElementById("thread-password-input").value;
+    if (!thread_title || !thread_password) {
+        alert("Thread title and password to delete are required");
+        return;
+    };
+    const response = await fetch(`https://chunk-messageboard-09594f5bef7e.herokuapp.com/api/threads/${board}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: {
+            text: thread_title,
+            delete_password: thread_password
+        }
+    });
+    const data = await response.json();
+    document.getElementById("threads-container").innerHTML += 
+        `<div class="thread-container">
+            <span class="thread-title">
+                <strong>
+                    <a href="https://chunk-messageboard-09594f5bef7e.herokuapp.com/b/${board}/${data.thread._id}">
+                        ${data.thread.text}
+                    </a>
+                </strong>
+            </span>
+            <div class="thread-subcontainer">
+                <div class="created-on-container">
+                    <span>
+                        Created on 
+                    </span>
+                    <span class="thread-created-on">
+                        ${data.thread.created_on}
+                    </span>
+                </div>
+                <div class="bumped-on-container">
+                    <span>
+                        Bumped on 
+                    </span>
+                    <span class="thread-bumped-on">
+                        ${data.thread.bumped_on}
+                    </span>
+                </div>
+            </div>
+        </div>`;
+    thread_title = "";
+    thread_password = "";
 });
---
+++ public/thread.js
@@ -0,0 +1,79 @@
+const path = window.location.pathname;
+const match = path.match(/\/b\/([^\/]+)\/([^\/]+)/);
+const board = match[1];
+const thread_id = match[2];
+
+const report_function = async (replyId) => {
+    try {
+        const response = await fetch(`https://chunk-messageboard-09594f5bef7e.herokuapp.com/api/replies/${board}`, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: {
+                thread_id,
+                reply_id: replyId
+            }
+        });
+        const data = await response.json();
+        if (JSON.stringify(data) === "reported") {
+            document.getElementById(`report-btn-${replyId}`).disabled = true;
+            document.getElementById(`report-btn-${replyId}`).innerText = "Reported";
+        } else {
+            console.log("Error: server response not 'reported'");
+            alert("Error: server response not 'reported'");
+        };
+    } catch (err) {
+        console.log("Server error:", err);
+        alert("Server error:", err);        
+    };
+};
+
+document.addEventListener("DOMContentLoaded", async () => {
+    document.getElementById("h1-dynamic-child").innerText = board;
+    try {
+        const response = await fetch(`https://chunk-messageboard-09594f5bef7e.herokuapp.com/api/replies/${board}`);
+        const data = await response.json();
+        document.getElementById("h2-dynamic-child").innerText = data.text;
+        data.replies.forEach(reply => {
+            document.getElementById("replies-container").innerHTML +=
+                `<div class="reply-container">
+                    <div class="reply-date-container">
+                        <strong>Posted on <span class="reply-date-span">${reply.created_on}</span></strong>
+                    </div>
+                    <div class="reply-text">
+                        ${reply.text}
+                    </div>
+                    <button class="report-btn" id="report-btn-${reply._id}" onclick="report_function(${reply._id})">
+                        Report
+                    </button>
+                </div>`
+        });
+    } catch (err) {
+        console.log("Server error:", err);
+        alert("Server error:", err)
+    }
+});
+
+document.getElementById("post-reply-btn").addEventListener("click", async () => {
+    const reply_text = document.getElementById("reply-text-input").value;
+    const response = await fetch(`https://chunk-messageboard-09594f5bef7e.herokuapp.com/api/replies/${board}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: {
+            thread_id,
+            text: reply_text,
+        }
+    });
+    const data = response.json();
+    document.getElementById("replies-container").innerHTML +=
+        `<div class="reply-container">
+            <div class="reply-date-container">
+                <strong>Posted on <span class="reply-date-span">${data.reply.created_on}</span></strong>
+            </div>
+            <div class="reply-text">
+                ${data.reply.text}
+            </div>
+            <button class="report-btn" id="report-btn-${data.reply._id}" onclick="report_function(${data.reply._id})">
+                Report
+            </button>
+        </div>`
+});
